18d3e4b042b8a4a68505e66d9d983a77b05ef08c
Konrad Zagozda <zagozdakonrad@gmail.com>
2023-04-27 19:13:19 +0200

    add to .gitignore

--- run-cmd.sh
+++ run-cmd.sh
@@ -1,4 +1,6 @@
-# usage: ./run-cmd.sh pytest
+# usage: ./run-cmd.sh <command>
+# 	e.g. ./run-cmd.sh pytest -n 8
 # prerequisite: running stack with "docker compose up"
+
 
 docker compose run -it backend "$*"
