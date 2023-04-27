e400f6d3c57a4bf81f93ca4ece9195d18e2d9ec8
Konrad Zagozda <zagozdakonrad@gmail.com>
2023-04-27 21:50:20 +0200

    add 100% coverage

--- backend/todo/tests.py
+++ backend/todo/tests.py
@@ -1,34 +1,91 @@
 import pytest
 from django.contrib.auth.models import User
+from todo.serializers import TodoSerializer
 from .models import Todo
+from rest_framework.test import APIClient
 
 pytestmark = pytest.mark.django_db
 
-# test models
 @pytest.fixture
 def test_user():
     return User.objects.create_user(username='test_user', password='test_password')
 
-def test_todo_creation(test_user):
-    todo = Todo.objects.create(
-        user=test_user,
-        title='Test Todo',
-        description='This is a test Todo item.',
-        completed=False
-    )
+@pytest.fixture
+def api_client():
+    return APIClient()
 
-    assert todo.user == test_user
-    assert todo.title == 'Test Todo'
-    assert todo.description == 'This is a test Todo item.'
-    assert todo.completed == False
-    assert str(todo) == 'Test Todo'
+class TestTodoModel:
+    def test_todo_creation(self, test_user):
+        todo = Todo.objects.create(
+            user=test_user,
+            title='Test Todo',
+            description='This is a test Todo item.',
+            completed=False
+        )
 
-def test_todo_completed(test_user):
-    todo = Todo.objects.create(
-        user=test_user,
-        title='Test Todo',
-        description='This is a test Todo item.',
-        completed=True
-    )
+        assert todo.user == test_user
+        assert todo.title == 'Test Todo'
+        assert todo.description == 'This is a test Todo item.'
+        assert todo.completed == False
+        assert str(todo) == 'Test Todo'
 
-    assert todo.completed == True
+    def test_todo_completed(self, test_user):
+        todo = Todo.objects.create(
+            user=test_user,
+            title='Test Todo',
+            description='This is a test Todo item.',
+            completed=True
+        )
+
+        assert todo.completed == True
+
+class TestTodoSerializer:
+    def test_todo_serializer(self, test_user):
+            todo = Todo.objects.create(
+                title='Test Title',
+                description='Test Description',
+                completed=False,
+                user=test_user
+            )
+
+            serializer = TodoSerializer(todo)
+
+            assert serializer.data == {
+                'id': todo.id,
+                'title': 'Test Title',
+                'description': 'Test Description',
+                'completed': False
+            }
+
+class TestTodoViewSet:
+    def test_todo_viewset_list(self, test_user, api_client):
+
+        Todo.objects.create(user=test_user, title='Test Todo', description='This is a test Todo item.', completed=False)
+        api_client.force_authenticate(user=test_user)
+
+        response = api_client.get('/api/todos/')
+
+        assert response.status_code == 200
+        assert len(response.data) == 1
+        assert response.data[0]['title'] == 'Test Todo'
+        assert response.data[0]['description'] == 'This is a test Todo item.'
+        assert response.data[0]['completed'] == False
+
+    def test_todo_viewset_create(self, test_user, api_client):
+        api_client.force_authenticate(user=test_user)
+
+        response = api_client.post('/api/todos/', data={
+            'title': 'Test Todo',
+            'description': 'This is a test Todo item.',
+            'completed': False,
+        })
+
+        assert response.status_code == 201
+        assert response.data['title'] == 'Test Todo'
+        assert response.data['description'] == 'This is a test Todo item.'
+        assert response.data['completed'] == False
+
+        todo = Todo.objects.first()
+        assert todo.title == 'Test Todo'
+        assert todo.description == 'This is a test Todo item.'
+        assert todo.completed == False
